c73cf969d071573ced7e4ace5e15d23cf8746f25
annacb21 <annacb21@gmail.com>
2020-06-05 15:14:35 +0200

    Sistemato file sudinoi.php

--- public/sudinoi.php
+++ public/sudinoi.php
@@ -1,32 +1,5 @@
 <?php require_once("../resources/config.php"); ?>
-
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="it" lang="it">
-<head>
-    <!-- meta tag -->
-    <meta http-equiv="content-type" content="text/html" charset="utf-8">
-    <meta name="description" content="Bisogna inserire la descrizione breve del contenuto della pagina">
-    <meta name="keywords" content="Bisogna inserire delle parole chiave che compaiano nel contenuto della pagina">
-    <meta name="author" content="Anna Cisotto Bertocco, Ismaele Gobbo, Paolo Di Dio, Lucrezia Gradi">
-
-    <!-- tag di Paolo per non avere la pagina in cache -->
-    <meta http-equiv="Cache-control" content="no-cache">
-
-
-    <title>Su di noi</title>
-
-    <link rel="stylesheet" type="text/css" href="css/styles.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
-
-
-</head>
-
-<body>
-
-<!-- Navigation bar-->
-<?php include(TEMPLATE_FRONT . DS . "navbar.php"); ?>
+<?php include(TEMPLATE_FRONT . DS . "intro.php"); ?>
 
 
 <div class="header">
@@ -35,3 +8,5 @@
         <p>Scrivi un storiella moderatamente a caso</p>
     </div>
 </div>
+
+<?php include(TEMPLATE_FRONT . DS . "footer.php"); ?>
