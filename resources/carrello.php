a78e193f82abee9e9c50dc81ee5400ffb112d002
annacb21 <annacb21@gmail.com>
2020-06-05 15:16:02 +0200

    Sistemato file carrello.php

--- resources/carrello.php
+++ resources/carrello.php
@@ -1,7 +1,8 @@
-<?php require_once("../resources/config.php"); ?>
+<?php require_once("config.php"); ?>
 
 <?php
 
+// aggiunta di una pianta al carrello
 if(isset($_GET['add'])) {
 
     $query = query("SELECT * FROM piante WHERE pianta_id=" . escape_string($_GET['add']) . " ");
@@ -11,43 +12,55 @@ if(isset($_GET['add'])) {
 
         if($row['pianta_qt'] != $_SESSION['product_' . $_GET['add']]){
             $_SESSION['product_' . $_GET['add']] += 1;
-            redirect("checkout.php");
+            redirect("../public/checkout.php");
         }
         else {
             set_message("Sono rimaste solo " . $row['pianta_qt' . " piante"]);
-            redirect("checkout.php");
+            redirect("../public/checkout.php");
         }
 
     }
 
 }
 
+// decremento della quantità di una pianta nel carrello
 if(isset($_GET['remove'])) {
 
     $_SESSION['product_' . $_GET['remove']]--;
 
     if($_SESSION['product_' . $_GET['remove']] < 1) {
-        redirect("checkout.php");
+        unset($_SESSION['item_total']);
+        unset($_SESSION['item_quantity']);
+        redirect("../public/checkout.php");
     }
     else {
-        redirect("checkout.php");
+        redirect("../public/checkout.php");
     }
 
 }
 
-
+// rimozione di una pianta dal carrello
 if(isset($_GET['delete'])) {
 
     $_SESSION['product_' . $_GET['delete']] = 0;
 
-    redirect("checkout.php");
+    unset($_SESSION['item_total']);
+    unset($_SESSION['item_quantity']);
+
+    redirect("../public/checkout.php");
 
 }
 
+// ritorna il riepilogo del carrello
 function cart() {
 
 $total = 0;
 $tot_quant = 0;
+
+$item_name = 1;
+$item_number = 1;
+$amount = 1;
+$quantity = 1;
 
 foreach($_SESSION as $name => $value) {
 
@@ -75,12 +88,19 @@ $product = <<<DELIMETER
     <td>{$row['prezzo']}</td>
     <td>{$value}</td>
     <td>{$sub}</td>
-    <td><a href="carrello.php?remove={$row['pianta_id']}">Remove</a><a href="carrello.php?add={$row['pianta_id']}">Add</a><a href="carrello.php?delete={$row['pianta_id']}">Delete</a></td>
+    <td><a href="../resources/carrello.php?remove={$row['pianta_id']}">Remove</a><a href="../resources/carrello.php?add={$row['pianta_id']}">Add</a><a href="../resources/carrello.php?delete={$row['pianta_id']}">Delete</a></td>
 </tr>
 
 DELIMETER;
 
 echo $product;
+
+$item_name++;
+$item_number++;
+$amount++;
+$quantity++;
+
+}
 
 $_SESSION['item_total'] = $total += $sub;
 $_SESSION['item_quantity'] = $tot_quant;
@@ -93,7 +113,23 @@ $_SESSION['item_quantity'] = $tot_quant;
 
 }
 
+// mostra il bottone di acquisto 
+function pay() {
+
+if(isset($_SESSION['item_quantity']) && $_SESSION['item_quantity'] >= 1) {
+
+$payment_button = <<<DELIMETER
+
+<a class="btn btn-primary" href="conferma.php?amount={$_SESSION['item_total']}">Conferma pagamento</a>
+
+DELIMETER;
+
+echo $payment_button;
+
 }
+
+}
+
 
 
 
