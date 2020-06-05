dfd555100279fe4c5e05e2708373f8216b5779a8
annacb21 <annacb21@gmail.com>
2020-06-05 15:26:27 +0200

    Sistemato file navbar.php

--- resources/templates/front/navbar.php
+++ resources/templates/front/navbar.php
@@ -1,11 +1,18 @@
+
 <div class="navigation-bar" id="Navbar">
   <a href="../public/index.php">Home</a>
   <a href="../public/piante.php">Piante</a>
   <a href="../public/sudinoi.php">Su di noi</a>
   <a href="../public/contatti.php">Contatti</a>
   <a href="../public/checkout.php">Carrello</a>
-  <a href="../public/registrazione.php" id="Registrati">Registrati</a>
-  <a href="../public/login.php" id="Accedi">Accedi</a>
+  <?php if(isset($_SESSION['user']) && !empty($_SESSION['user'])) {
+        echo "<a href=\"profile.php\">Profilo</a>";
+        echo "<a href=\"logout.php\" id='logout'>Logout</a>";}
+    else{
+        echo "<a href=\"login.php\" id=\"Accedi\">Accedi</a>";
+        echo "<a href='registrati.html' id='Registrati'>Registrati</a>";
+      }
+    ?>
   <a href="javascript:void(0);" class="icon" onclick="myFunction()">
     <i class="fa fa-bars"></i>
   </a>
