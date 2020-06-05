5b03af95789029251cb7588cf7187986acf87023
annacb21 <annacb21@gmail.com>
2020-06-05 15:28:26 +0200

    Aggiunto file intro.php

---
+++ resources/templates/front/intro.php
@@ -0,0 +1,25 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="it" lang="it">
+
+<head>
+
+  <!-- meta tag -->
+  <meta http-equiv="content-type" content="text/html" charset="utf-8">
+  <meta name="description" content="Bisogna inserire la descrizione breve del contenuto della pagina">
+  <meta name="keywords" content="succulente, piante">
+  <meta name="author" content="Anna Cisotto Bertocco, Ismaele Gobbo, Paolo di Dio, Lucrezia Gradi">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- tag di Paolo per non avere la pagina in cache -->
+  <meta http-equiv="Cache-control" content="no-cache">
+
+  <title>Succulente</title>
+  <link rel="stylesheet" type="text/css" href="css/styles.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+
+<body>
+
+  <?php include(TEMPLATE_FRONT . DS . "navbar.php"); ?>
