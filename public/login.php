10d1d3601669e4d599ba07da299746740fd91ee8
annacb21 <annacb21@gmail.com>
2020-06-05 15:06:49 +0200

    Sistemato file login.php

--- public/login.php
+++ public/login.php
@@ -1,31 +1,5 @@
 <?php require_once("../resources/config.php"); ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="it" lang="it">
-
-<head>
-    <!-- meta tag -->
-    <meta http-equiv="content-type" content="text/html" charset="utf-8">
-    <meta name="description" content="Bisogna inserire la descrizione breve del contenuto della pagina">
-    <meta name="keywords" content="Bisogna inserire delle parole chiave che compaiano nel contenuto della pagina">
-    <meta name="author" content="Anna Cisotto Bertocco, Ismaele Gobbo, Paolo Di Dio, Lucrezia Gradi">
-
-    <!-- tag di Paolo per non avere la pagina in cache -->
-    <meta http-equiv="Cache-control" content="no-cache">
-
-
-    <title>Login utenti</title>
-
-    <link rel="stylesheet" type="text/css" href="css/styles.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
-
-
-</head>
-
-<body>
-
-<!-- Navigation bar-->
-<?php include(TEMPLATE_FRONT . DS . "navbar.php"); ?>
+<?php include(TEMPLATE_FRONT . DS . "intro.php"); ?>
 
 
 <div class="header">
@@ -37,26 +11,24 @@
 
 <div>
     <form method="post" action="">
-
+      <legend><h3>Accedi</h3></legend>
       <?php login_user(); ?>
         <div class="input-group">
-            <label for="">Email</label>
+            <label for="email">Email: </label>
             <input type="email" name="email" value="">
         </div>
         <div class="input-group">
-            <label for="password">Password</label>
+            <label for="password">Password: </label>
             <input type="password" name="password">
         </div>
         <div class="input-group">
-            <button type="submit" class="btn" name="log_user">Login</button>
+            <button type="submit" class="btn" name="reg_user">Login</button>
+        </div>
+        <div>
+            <a href="#" > Non hai un account?</a>
         </div>
 
     </form>
 </div>
 
-
-
-<script src="js/hamburger.js"></script>
-
-</body>
-</html>
+<?php include(TEMPLATE_FRONT . DS . "footer.php"); ?>
