a4fe4cc8c51eecb2e5881c6050718bc5d3ce4aa5
annacb21 <annacb21@gmail.com>
2020-06-05 14:59:11 +0200

    Aggiunto file categorie.php

---
+++ public/categorie.php
@@ -0,0 +1,23 @@
+<?php require_once("../resources/config.php"); ?>
+<?php include(TEMPLATE_FRONT . DS . "intro.php"); ?>
+
+<div id="plant-page">
+
+    <div id="plants">
+
+      <div id="plant-flex-wrapper">
+
+        <h3>Lista delle nostre piante</h3>
+        <ul class="plant-flex">
+
+          <?php get_cat_products(); ?>
+
+        </ul>
+
+      </div>
+
+    </div>
+
+</div>
+
+<?php include(TEMPLATE_FRONT . DS . "footer.php"); ?>
