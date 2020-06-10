b98b6905366a63c9edf8128c43ca312dc451969f
annacb21 <annacb21@gmail.com>
2020-06-10 10:58:26 +0200

    aggiunto controllo registrazione

--- public/registrazione.php
+++ public/registrazione.php
@@ -2,11 +2,12 @@
 <?php include(TEMPLATE_FRONT . DS . "intro.php"); ?>
 
 <div class="register-page">
+
     <h1 class="page-title">Registrati</h1>
     <p><?php display_message(); ?></p>
 
     <form method="post" action="">
-        <legend>Accedi</legend>
+        <legend>Registrati</legend>
         <?php reg_user(); ?>
         <div class="reg-input-group">
             <label for="email">Email: </label>
@@ -14,15 +15,34 @@
         </div>
         <div class="reg-input-group">
             <label for="password">Password: </label>
-            <input type="password" name="password">
+            <input type="password" name="password" id="password" onkeyup="check();">
         </div>
         <div class="reg-input-group">
             <label for="password">Ripeti password: </label>
-            <input type="password" name="password2">
+            <input type="password" name="password2" id="password2" onkeyup="check();">
+            <span id='message'></span>
         </div>
         <button type="submit" name="reg_user">Registrati</button>
 
+        <!-- controllo input lato client -->
+        <script>
+
+            var check = function() {
+                if (document.getElementById('password').value ===
+                    document.getElementById('password2').value) {
+
+                    document.getElementById('message').style.color = 'green';
+                    document.getElementById('message').innerHTML = 'Le password corrispondono';
+                } else {
+                    document.getElementById('message').style.color = 'red';
+                    document.getElementById('message').innerHTML = 'Le password non corrispondono';
+                }
+            }
+
+        </script>
+
     </form>
+
 </div>
 
 
