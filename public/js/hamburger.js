ac0449a2658c2cae536957312da0c41361ee4e83
annacb21 <annacb21@gmail.com>
2020-06-23 18:50:00 +0200

    sistemata navbar e sidebar admin

--- public/js/hamburger.js
+++ public/js/hamburger.js
@@ -1,5 +1,14 @@
 function myFunction() {
   var x = document.getElementById("Navbar");
+  if (x.className == "navigation-bar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navigation-bar";
+  }
+}
+
+function myFunction1() {
+  var x = document.getElementById("AdminNavbar");
   if (x.className === "navigation-bar") {
     x.className += " responsive";
   } else {
--- resources/templates/back/navbar.php
+++ resources/templates/back/navbar.php
@@ -1,18 +1,22 @@
-<div class="admin-navbar" id="Navbar">
-  <a href="../index.php" xml:lang="en">Home</a>
-  <a href="logout.php" id="Logout" xml:lang="en">Logout</a>
-  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-    <i class="fa fa-bars"></i>
-  </a>
+<div class="navigation-bar" id="AdminNavbar">
+  <ul>
+    <li><a href="../index.php" xml:lang="en" tabindex="1">Home</a></li>
+    <li><a href="logout.php" id="AdminLogout" xml:lang="en" tabindex="2">Logout</a></li>
+    <li class="icon"><a href="javascript:void(0);" onclick="myFunction1()">
+      <i class="fa fa-bars"></i>
+    </a></li>
+  </ul>
 </div>
 
 <!-- side nav bar -->
 <div class="sidenav">
-  <a href="index.php" xml:lang="en">Dashboard</a>
-  <a href="index.php?orders">Ordini</a>
-  <a href="index.php?reports">Reports</a>
-  <a href="index.php?prodotti">Prodotti</a>
-  <a href="index.php?add-product">Aggiungi prodotto</a>
-  <a href="index.php?add-categoria">Categorie</a>
-  <a href="index.php?users">Utenti</a>
+  <ul>
+    <li><a href="index.php" xml:lang="en">Dashboard</a></li>
+    <li><a href="index.php?orders">Ordini</a></li>
+    <li><a href="index.php?reports">Reports</a></li>
+    <li><a href="index.php?prodotti">Prodotti</a></li>
+    <li><a href="index.php?add-product">Aggiungi prodotto</a></li>
+    <li><a href="index.php?add-categoria">Categorie</a></li>
+    <li><a href="index.php?users">Utenti</a></li>
+  </ul>
 </div>
