25a5a6e9a24ed381faa5172e844c6f44addd057a
annacb21 <annacb21@gmail.com>
2020-06-25 17:35:49 +0200

    aggiunto tag fieldset nei form

--- public/login.php
+++ public/login.php
@@ -14,18 +14,19 @@
     <p class="warning-msg"><?php display_message(); ?></p>
 
     <form method="post" action="">
-        <legend>Accedi</legend>
-        <?php login_user(); ?>
-        <div class="login-input-group">
-            <label for="email" xml:lang="en">Email: </label>
-            <input type="email" placeholder="Inserisci email" name="email" required>
-            <label for="password" xml:lang="en">Password: </label>
-            <input type="password" placeholder="Inserisci password" name="password" required>
-        </div>
-        <button class="submit-button" type="submit" name="log_user">Accedi</button>
-        <input class="reset-button" type="button" value="Reset" onClick="this.form.reset()" />
-        <a href="../public/registrazione.php"> Non hai un <span xml:lang="en">account</span>? Registrati</a>
-
+        <fieldset>
+            <legend>Accedi</legend>
+            <?php login_user(); ?>
+            <div class="login-input-group">
+                <label for="email" xml:lang="en">Email: </label>
+                <input type="email" placeholder="Inserisci email" name="email" required>
+                <label for="password" xml:lang="en">Password: </label>
+                <input type="password" placeholder="Inserisci password" name="password" required>
+            </div>
+            <button class="submit-button" type="submit" name="log_user">Accedi</button>
+            <input class="reset-button" type="button" value="Reset" onClick="this.form.reset()" />
+            <a href="../public/registrazione.php"> Non hai un <span xml:lang="en">account</span>? Registrati</a>
+        </fieldset>
     </form>
 </div>
 
--- public/registrazione.php
+++ public/registrazione.php
@@ -16,37 +16,38 @@
     <p class="warning-msg"><?php display_message(); ?></p>
 
     <form method="post" action="">
-        <legend>Registrati</legend>
-        <?php reg_user(); ?>
-        <div class="login-input-group">
-            <label for="email" xml:lang="en">Email: </label>
-            <input type="email" placeholder="Inserisci email" name="email" required>
-            <label for="password"  xml:lang="en">Password: </label>
-            <input type="password" placeholder="Inserisci password" name="password" id="password" onkeyup="check();" required>
-            <label for="password">Ripeti <span  xml:lang="en">password:</span>  </label>
-            <input type="password" placeholder="Inserisci nuovamente la password" name="password2" id="password2" onkeyup="check();" required>
-            <span id='message'></span>
-        </div>
-        <button class="submit-button" type="submit" name="reg_user">Registrati</button>
-        <input class="reset-button" type="button" value="Reset" onClick="this.form.reset()" />
+        <fieldset>
+            <legend>Registrati</legend>
+            <?php reg_user(); ?>
+            <div class="login-input-group">
+                <label for="email" xml:lang="en">Email: </label>
+                <input type="email" placeholder="Inserisci email" name="email" required>
+                <label for="password"  xml:lang="en">Password: </label>
+                <input type="password" placeholder="Inserisci password" name="password" id="password" onkeyup="check();" required>
+                <label for="password2">Ripeti <span  xml:lang="en">password:</span>  </label>
+                <input type="password" placeholder="Inserisci nuovamente la password" name="password2" id="password2" onkeyup="check();" required>
+                <span id='message'></span>
+            </div>
+            <button class="submit-button" type="submit" name="reg_user">Registrati</button>
+            <input class="reset-button" type="button" value="Reset" onClick="this.form.reset()" />
 
-        <!-- controllo input lato client -->
-        <script>
+            <!-- controllo input lato client -->
+            <script>
 
-            var check = function() {
-                if (document.getElementById('password').value ===
-                    document.getElementById('password2').value) {
+                var check = function() {
+                    if (document.getElementById('password').value ===
+                        document.getElementById('password2').value) {
 
-                    document.getElementById('message').style.color = 'green';
-                    document.getElementById('message').innerHTML = 'Le password corrispondono';
-                } else {
-                    document.getElementById('message').style.color = 'red';
-                    document.getElementById('message').innerHTML = 'Le password non corrispondono';
+                        document.getElementById('message').style.color = 'green';
+                        document.getElementById('message').innerHTML = 'Le password corrispondono';
+                    } else {
+                        document.getElementById('message').style.color = 'red';
+                        document.getElementById('message').innerHTML = 'Le password non corrispondono';
+                    }
                 }
-            }
 
-        </script>
-
+            </script>
+        </fieldset>
     </form>
 
 </div>
