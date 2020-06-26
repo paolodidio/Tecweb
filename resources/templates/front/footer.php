0645253974db3d84796851c30b8f733dafea8fed
annacb21 <annacb21@gmail.com>
2020-06-26 11:53:06 +0200

    fatto footer

--- resources/templates/front/footer.php
+++ resources/templates/front/footer.php
@@ -1,3 +1,13 @@
+<div class="footer">
+    <dl>
+        <dt>Autori:</dt>
+        <dd>Anna Cisotto Bertocco, Lucrezia Gradi, Paolo di Dio, Ismaele Gobbo</dd>
+        <dt>Ultimo aggiornamento</dt>
+        <dd>Venerdì 26 giugno 2020</dd>
+    </dl>
+    <p>&copy;2020 tutti i diritti riservati</p>
+</div>
+
 <script src="../public/js/hamburger.js" type="text/javascript"></script>
 
 </body>
